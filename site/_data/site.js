0e58794a695ecb3473e53c5a7d3c1868ad10a890
Ben Michaud <ben@tigersway.net>
2020-07-28 10:37:09 +0700

    Sitemap

--- site/_data/site.js
+++ site/_data/site.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: "ECBS",
   tagline: "Eleventy Classic Blog Starter",
+  url: "https://eleventy-classic-blog-starter.netlify.app",
   time: Date.now(),
   locale: "en-GB",
   theme: "hyde"
