346546106be26d634d4df184b3ab60e140a23c12
Ben Michaud <ben@tigersway.net>
2021-12-27 14:33:17 +0700

    Netlify URL

--- site/_data/site.js
+++ site/_data/site.js
@@ -1,7 +1,8 @@
 module.exports = {
   title: "ECBS",
   tagline: "Eleventy Classic Blog Starter",
-  url: "https://eleventy-classic-blog-starter.netlify.app",
+  // url: "https://eleventy-classic-blog-starter.netlify.app",
+  url: process.env.URL,
   time: Date.now(),
   locale: "en-GB",
   theme: "hyde"
