09941083e3a4dca2b85c261362748121c52cdd75
Ben Michaud <ben@tigersway.net>
2022-12-20 08:20:13 +0700

    deploy tests

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,19 @@
+require('dotenv').config();
+
 const
   glob = require('fast-glob'),
   hljs = require('highlight.js'),
-  sizeOf = require('image-size');
+  sizeOf = require('image-size'),
 
-const
-  site = require('./site/_data/site.js'),
-  theme = '_themes/' + (process.env.THEME || site.theme);
+  projectName = process.env.npm_package_name,
+  theme = process.env.npm_package_config_theme;
+// site = require('./site/_data/site.js'),
+// theme = '_themes/' + (process.env.THEME || site.theme);
 
 
 module.exports = (eleventyConfig) => {
 
-  // Markdown engine with its plugins
+  // Engine: Markdown & plugins
   const Markdown = require('markdown-it')({
     html: true,         // Enable HTML tags in source
     breaks: true,       // Convert '\n' in paragraphs into <br>
@@ -21,9 +24,9 @@ module.exports = (eleventyConfig) => {
       if (lang && hljs.getLanguage(lang)) {
         try {
           return '<pre class="hljs"><code>' +
-               hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
-               '</code></pre>';
-        } catch (__) {}
+            hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+            '</code></pre>';
+        } catch (e) { }
       }
 
       return '';
@@ -39,83 +42,97 @@ module.exports = (eleventyConfig) => {
     })
     .use(require('markdown-it-attrs'), {
       allowedAttributes: ['id', 'class']
-    });
+    })
+    // .use(require('markdown-it-eleventy-img'), {
+    //   imgOptions: {
+    //     widths: [720, 1440],
+    //     urlPath: '/images/',
+    //     outputDir: (process.env.NODE_ENV === 'production') ? './build/images' : './public/images'
+    //   },
+    //   // globalAttributes: { sizes: '(min-width:768px) calc(min(calc(100vw - 22rem), 38rem) - 2rem), calc(min(100vw, 38rem) - 2rem)' },
+    //   globalAttributes: { sizes: '(min-width:768px) 720px, 100vw' },
+    //   // globalAttributes: { sizes: '100vw' },
+    //   // renderImage(image, attributes) {
+    //   //   const [Image, options] = image;
+    //   //   const [src, attrs] = attributes;
+    //   //   console.log(src, attrs);
+    //   // },
+    //   resolvePath(src, env) {
+    //     // console.log('Path', src, env.page, env.page.inputPath.split('/').slice(0, -1).concat(src).join('/'));
+    //     return env.page.inputPath.split('/').slice(0, -1).concat(src).join('/');
+    //   }
+    // })
+    ;
   eleventyConfig.setLibrary('md', Markdown);
+
+  // Engine: Nunjucks
+  eleventyConfig.setNunjucksEnvironmentOptions({ trimBlocks: true, lstripBlocks: true });
 
 
   // Filters
-  glob.sync(['_11ty/filters/*.js', `site/${theme}\filters.js`]).forEach(file => {
+  glob.sync('./site/_filters/*.js').forEach(file => {
     let filters = require('./' + file);
     Object.keys(filters).forEach(name => eleventyConfig.addFilter(name, filters[name]));
   });
 
   // Shortcodes
-  glob.sync(['_11ty/shortcodes/*.js', `site/${theme}\shortcodes.js`]).forEach(file => {
+  glob.sync('./site/_shortcodes/*.js').forEach(file => {
     let shortcodes = require('./' + file);
     Object.keys(shortcodes).forEach(name => eleventyConfig.addShortcode(name, shortcodes[name]));
   });
 
   // PairedShortcodes
-  glob.sync(['_11ty/pairedShortcodes/*.js', `site/${theme}\pairedShortcodes.js`]).forEach(file => {
+  glob.sync('./site/_pairedShortcodes/*.js').forEach(file => {
     let pairedShortcodes = require('./' + file);
     Object.keys(pairedShortcodes).forEach(name => eleventyConfig.addPairedShortcode(name, pairedShortcodes[name]));
   });
 
+
   // Collections
-  eleventyConfig.addCollection('pages', (collectionApi) => collectionApi.getFilteredByGlob('site/pages/**/*.md'));
-  eleventyConfig.addCollection('posts', (collectionApi) => collectionApi.getFilteredByGlob('site/posts/**/*.md'));
+  eleventyConfig.addCollection('pages', (collectionApi) => collectionApi.getFilteredByGlob('./site/pages/**/*.md'));
+  eleventyConfig.addCollection('posts', (collectionApi) => collectionApi.getFilteredByGlob('./site/posts/**/*.md'));
+
 
   // Transforms
-  const {suffix} = require('./_11ty/filters/rename.js');
-  eleventyConfig.addTransform('async-transform-images', async (fileContent, outputPath, inputPath) => {
-    if (outputPath.endsWith('.html')) {
-      let lazy = 0;
-      return fileContent.replace(/(<article .*?data-input-path="(.*?)".*?>)([\s\S]*?)(<\/article>)/gim, (match, openingtag, inputPath, content, closingtag) => {
-        let imagePath = inputPath.match(/(?:\/posts(\/\d{4}\/\d{2}\/)|\/pages\/)[^\/]*/);
-        imagePath = (imagePath) ? (imagePath[1] ? imagePath[1] : '/') : '';
-        content = content.replace(/<img src="(?!https?:\/\/)(.*?).jpg" alt="(.*?)">/g, (match, src, alt) => {
-          const size = sizeOf(`site/posts/${imagePath}${src}.jpg`)
-          // return `<picture>
-          // <source type="image/webp" srcset="${suffix(`/images${imagePath}${src}.webp`, '-330x')} 330w, ${suffix(`/images${imagePath}${src}.webp`, '-720x')} 720w, ${suffix(`/images${imagePath}${src}.webp`, '-330x@2x')} 2x, ${suffix(`/images${imagePath}${src}.webp`, '-330x@3x')} 3x">
-          // <img src="${suffix(`/images${imagePath}${src}.jpg`, '-720x')}" alt="${alt}"${lazy++ ? ' loading="lazy"' : ''} width="1920" height="1080">
-          // </picture>`
-          return `<img src="${suffix(`/images${imagePath}${src}.jpg`, '-720x')}" `
-            +`srcset="${suffix(`/images${imagePath}${src}.jpg`, '-360x')} 360w, ${suffix(`/images${imagePath}${src}.jpg`, '-720x')} 720w, ${suffix(`/images${imagePath}${src}.jpg`, '-1080x')} 1080w, ${suffix(`/images${imagePath}${src}.jpg`, '-1440x')} 1440w"
-            sizes="(min-width:768px) calc(min(calc(100vw - 22rem), 38rem) - 2rem), calc(min(100vw, 38rem) - 2rem)"
-            alt="${alt}"${lazy++ ? ' loading="lazy"' : ''} width="${size.width}" height="${size.height}">`;
-        })
-        return `${openingtag}${content}${closingtag}`;
-      });
-    }
-    return fileContent;
-  });
+  // const {suffix} = require('./_11ty/filters/rename.js');
+  // eleventyConfig.addTransform('async-transform-images', async (content, outputPath) => {
+  //   if (outputPath && outputPath.endsWith(".html")) {
+  //     let lazy = 0;
+  //     return content.replaceAll(/(<article .*?data-input-path="(.*?)".*?>)([\s\S]*?)(<\/article>)/gim, (match, openingtag, inputPath, content, closingtag) => {
+  //       // console.log(outputPath, openingtag, inputPath);
+  //       inputPath = inputPath.split('/').slice(2, -1).join('/');
+  //       content = content.replaceAll(/<img src="((?!https?:\/\/).*?.jpg)" alt="(.*?)">/g, (match, src, alt) => {
+  //         const { width, height } = sizeOf(`site/posts/${inputPath}/${src}`);
 
+  //         //         const size = sizeOf(`site/posts/${imagePath}${src}.jpg`)
+  //         //         // return `<picture>
+  //         //         // <source type="image/webp" srcset="${suffix(`/images${imagePath}${src}.webp`, '-330x')} 330w, ${suffix(`/images${imagePath}${src}.webp`, '-720x')} 720w, ${suffix(`/images${imagePath}${src}.webp`, '-330x@2x')} 2x, ${suffix(`/images${imagePath}${src}.webp`, '-330x@3x')} 3x">
+  //         //         // <img src="${suffix(`/images${imagePath}${src}.jpg`, '-720x')}" alt="${alt}"${lazy++ ? ' loading="lazy"' : ''} width="1920" height="1080">
+  //         //         // </picture>`
+  //         //         return `<img src="${suffix(`/images${imagePath}${src}.jpg`, '-720x')}" `
+  //         //           +`srcset="${suffix(`/images${imagePath}${src}.jpg`, '-360x')} 360w, ${suffix(`/images${imagePath}${src}.jpg`, '-720x')} 720w, ${suffix(`/images${imagePath}${src}.jpg`, '-1080x')} 1080w, ${suffix(`/images${imagePath}${src}.jpg`, '-1440x')} 1440w"
+  //         //           sizes="(min-width:768px) calc(min(calc(100vw - 22rem), 38rem) - 2rem), calc(min(100vw, 38rem) - 2rem)"
+  //         //           alt="${alt}"${lazy++ ? ' loading="lazy"' : ''} width="${size.width}" height="${size.height}">`;
+  //         return `<img src="${process.env.CLOUDINARY_CDN_URL}/f_auto,q_40,w_720/${projectName}/${inputPath}/${src}" alt="${alt}" width="${width}" height="${height}">`;
+  //       });
+  //       return `${openingtag}${content}${closingtag}`;
+  //     });
+  //   }
+  //   return content;
+  // });
 
-  // Browser-Sync, Dev mode or "Passthrough File Copy"
+  eleventyConfig.addPassthroughCopy({ 'site/static': '.' });
+  eleventyConfig.addPassthroughCopy({ [`site/_themes/${theme}/static`]: '.' });
+  eleventyConfig.addPassthroughCopy({ 'node_modules/@fontsource/{abril-fatface,pt-sans}/files/{abril-fatface,pt-sans}-latin-{400,700}*.woff2': 'css/files' });
+  eleventyConfig.setServerPassthroughCopyBehavior("copy");
+
   if (process.env.NODE_ENV === 'production') {
 
-    eleventyConfig.addPassthroughCopy({ 'site/static': '.' });
-    eleventyConfig.addPassthroughCopy({ [`site/${theme}/static`]: '.' });
-
-  } else {
-
-    require('dotenv').config();
-
-    eleventyConfig.setBrowserSyncConfig({
-      browser: process.env.BROWSER || 'default',
-      port: process.env.PORT || 3000,
-      open: true,
-      serveStatic: [
-        { dir: 'site/static' },
-        { dir: `site/${theme}/static` }
-      ],
-      snippetOptions: {rule: {match: /<\/body>\n/i}}
-    });
 
   }
 
-  eleventyConfig.addWatchTarget(`site/${theme}/css`);
-  eleventyConfig.setWatchThrottleWaitTime(300);
+  // eleventyConfig.addWatchTarget(`site/${theme}/css`);
+  // eleventyConfig.setWatchThrottleWaitTime(300);
 
 
   return {
@@ -124,8 +141,8 @@ module.exports = (eleventyConfig) => {
 
     dir: {
       input: './site',
-      includes: `${theme}/layouts`,
+      includes: `_themes/${theme}/layouts`,
       output: './public'
     }
-  }
+  };
 };
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # eleventy-classic-blog-starter  ![GitHub last commit](https://img.shields.io/github/last-commit/tigersway/eleventy-classic-blog-starter?style=flat-square) ![GitHub issues](https://img.shields.io/github/issues/tigersway/eleventy-classic-blog-starter?style=flat-square)
 
-A starter repository for a classic blog website using the Eleventy SSG.
+A starter repository for a "classic-themed" blog website using Eleventy.
 
-Of course, a very opiniated one, and a work in progress!
+Of course, a very opinionated one, and probably forever, a work in progress!
 
 [![Cloudflare](https://img.shields.io/badge/Cloudflare_Pages--none?style=social&logo=cloudflare)](https://eleventy-classic-blog-starter.pages.dev/)
 [![Netlify](https://img.shields.io/badge/Netlify--none?style=social&logo=netlify)](https://eleventy-classic-blog-starter.netlify.app/)
@@ -11,63 +11,37 @@ Of course, a very opiniated one, and a work in progress!
 [![Surge](https://img.shields.io/badge/Surge--none?style=social&logo=eleventy&logoColor=def1e7)](https://eleventy-classic-blog-starter.surge.sh/)
 [![Hostman](https://img.shields.io/badge/Hostman--0959cd?style=social&logo=eleventy&logoColor=#0e103b)](https://eleventy-classic-blog-starter.hostman.site/)
 
+## Completely re-written v2
 
-## Local setup and run
-Very classic too...
-### Clone & install
-```
-git clone https://github.com/tigersway/eleventy-classic-blog-starter.git <my-blog-name>
-cd <my-blog-name>
-npm install
-```
-### build first, and set live!
-```
-npm run build
-npm run live
-```
+At the end of 2022, Gulp and its plugins seem to be gone; Eleventy has been updated quite a lot.
+It was time to clean-up this template!
 
 ## Tools
 
 - [Eleventy](https://www.11ty.dev/)
-- [Gulp](https://gulpjs.com/)
 - [highlight.js](https://highlightjs.org/)
-- [@tigersway/gulp-responsive](https://github.com/TigersWay/gulp-responsive) & [gulp-vinyl-flow](https://github.com/TigersWay/gulp-vinyl-flow)
+- [markdown-it-eleventy-img](https://github.com/solution-loisir/markdown-it-eleventy-img)
+- [Fontsource](https://fontsource.org/)
 
-## Themes
+## Theme
 
-- Nunjucks port - but same CSS - based on liquid layouts from [Hyde](https://hyde.getpoole.com/) by [@mdo](https://twitter.com/mdo)
-
-## Problems, notes and sometimes solutions
-
-### Dates!
-
-#### Creation (or Updated) dates
-
-Posts default dates (file creation) are only reliable in local, for you. Depending where you are, they may change during transfer, upload, etc.
-Differently but not really better, dates embedded inside the post filename (YYYY-MM-DD) only works in UTC, and will never - if needed - include a time of day.
-
-=> Conclusion: The only valid dates are coming from the "data cascade", from Frontmatter to computed data and work best in their complete form like this: "2020-07-20T13:00:00.000Z" or simplified like this: "2020-07-20 20:00:00 +7".
-
-#### SEO & Automatic link(s) to image(s)
-
-SEO experts are saying you should avoid to set year, month and day in your URL, which means you may have trouble keeping your images with their related post, and in the same time not keeping that "dated" structure for these same markdown document.
-
-=> Conclusion: As you build permalinks, you have to build links to images.
-
-### Responsive
-
-With a little help from [@tigersway/gulp-responsive](https://github.com/TigersWay/gulp-responsive) and [gulp-vinyl-flow](https://github.com/TigersWay/gulp-vinyl-flow), Eleventy can [transform](.eleventy.js#L76-L88) every `img` tag.
-
-### Nunjucks & Fragments
-
-[source](.eleventy.js#L42-L49)
+- Nunjucks port - with (nearly) same CSS - based on liquid layouts from [Hyde](https://hyde.getpoole.com/) by [@mdo](https://twitter.com/mdo)
+- Some WCAG contrast corrections
 
 ## Bug reports, feature requests... or even stars
-This is an ongoing project - even if I don't modify it everyday - and every suggestions and/or contributions are most welcome!
+
+This is an ongoing project - even if I don't modify it everyday - and every suggestions, contributions or even stars are most welcome!
 
 ## CHANGELOG
 
-**v1.0.0**
+- **v2.0.0**
+  - Update all dependencies
+  - eleventy-img plugin through markdown-it-eleventy-img
+  - Layout corrections
+  - css: postcss & fontsource
+  - spell-checker
+
+- **v1.0.0**
   - Lighthouse 4 x 100%
   - Color corrections (WCAG2AA)
   - RSS
@@ -79,6 +53,6 @@ This is an ongoing project - even if I don't modify it everyday - and every sugg
     - PostCSS
   - Delete FrontAid CMS files - CMS tests will be done later
 
-**v0.6.1**
+- **v0.6.1**
   - First try on "Cloudflare Pages"
   - Added robots.txt
